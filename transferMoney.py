4a4fb64691b6ccc06102f4870cdcbd9f9177dd8e
rainage1 <67071319+rainage1@users.noreply.github.com>
2021-01-10 02:09:54 -0500

    Create transferMoney.py

---
+++ transferMoney.py
@@ -0,0 +1,53 @@
+def chequingToSaving(transferAmount,rowNum,fields):
+
+  if int(fields[rowNum][8])-int(transferAmount) >= 0:
+    savingAmount = int(fields[rowNum][7])+int(transferAmount)
+    fields[rowNum][7] = str(savingAmount)
+    chequingAmount = int(fields[rowNum][8])-int(transferAmount)
+    fields[rowNum][8] = str(chequingAmount)
+    goalProgress = str(int(transferAmount)+int(fields[rowNum][9]))
+    fields[rowNum][9] = goalProgress
+
+    print("You transferred ${} into your savings account and only need ${} to reach your money management goal!".format(transferAmount, int(fields[rowNum][2]) - int(fields[rowNum][9])))
+
+
+    accountFile = open('clients.txt','w')
+
+    clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days\n'
+    
+    for row in range (len(fields)):
+      for column in range(11):
+        if column != 10:
+          clientList += fields[row][column] + ' '
+        else:
+          clientList+= fields[row][column]
+    accountFile.write(clientList)
+  else:
+    print("You do not have enough money to make the transfer.")
+
+  
+
+
+def savingToChequing(transferAmount,rowNum,fields):
+  if int(fields[rowNum][7])-int(transferAmount) >= 0:  
+    
+    savingAmount = int(fields[rowNum][7])-int(transferAmount)
+    fields[rowNum][7] = str(savingAmount)
+    
+    chequingAmount = int(fields[rowNum][8])+int(transferAmount)
+    fields[rowNum][8] = str(chequingAmount)
+      
+    accountFile = open('clients.txt','w')
+
+    clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days\n'
+    
+    for row in range (len(fields)):
+      for column in range(11):
+        if column != 10:
+          clientList += fields[row][column] + ' '
+        else:
+          clientList+= fields[row][column]
+
+    accountFile.write(clientList)
+  else:
+    print("You do not have enough money to make the transfer.")

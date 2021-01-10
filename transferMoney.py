ab869f14631d0a79cbadd7f4a9f343fd61e1417c
rainage1 <67071319+rainage1@users.noreply.github.com>
2021-01-10 09:59:22 -0500

    Update transferMoney.py

--- transferMoney.py
+++ transferMoney.py
@@ -1,3 +1,5 @@
+from datetime import datetime 
+
 def chequingToSaving(transferAmount,rowNum,fields):
 
   if int(fields[rowNum][8])-int(transferAmount) >= 0:
@@ -7,17 +9,17 @@ def chequingToSaving(transferAmount,rowNum,fields):
     fields[rowNum][8] = str(chequingAmount)
     goalProgress = str(int(transferAmount)+int(fields[rowNum][9]))
     fields[rowNum][9] = goalProgress
-
+    fields[rowNum][11] = str(datetime.now().day) + "\n"
     print("You transferred ${} into your savings account and only need ${} to reach your money management goal!".format(transferAmount, int(fields[rowNum][2]) - int(fields[rowNum][9])))
 
 
     accountFile = open('clients.txt','w')
 
-    clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days\n'
+    clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days | Last Plan Deposit Date\n'
     
     for row in range (len(fields)):
-      for column in range(11):
-        if column != 10:
+      for column in range(12):
+        if column != 11:
           clientList += fields[row][column] + ' '
         else:
           clientList+= fields[row][column]
@@ -39,11 +41,11 @@ def savingToChequing(transferAmount,rowNum,fields):
       
     accountFile = open('clients.txt','w')
 
-    clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days\n'
+    clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days | Last Plan Deposit Date\n'
     
     for row in range (len(fields)):
-      for column in range(11):
-        if column != 10:
+      for column in range(12):
+        if column != 11:
           clientList += fields[row][column] + ' '
         else:
           clientList+= fields[row][column]

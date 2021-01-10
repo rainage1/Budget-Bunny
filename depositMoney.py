1d3e2cea76bd020e84699f4a973e4dc8acd81449
rainage1 <67071319+rainage1@users.noreply.github.com>
2021-01-10 09:57:41 -0500

    Update depositMoney.py

--- depositMoney.py
+++ depositMoney.py
@@ -1,3 +1,5 @@
+from datetime import datetime 
+
 def chequingDeposit(depositAmount, rowNum, fields):
   accountFile = open('clients.txt','r')
   accountFile.close()
@@ -10,8 +12,8 @@ def chequingDeposit(depositAmount, rowNum, fields):
   clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days\n'
   
   for row in range (len(fields)):
-    for column in range(11):
-      if column != 10:
+    for column in range(12):
+      if column != 11:
         clientList += fields[row][column] + ' '
       else:
         clientList+= fields[row][column]
@@ -26,16 +28,18 @@ def savingDeposit(depositAmount, rowNum, fields):
   if fields[rowNum][2].isnumeric():
     goalProgress = str(int(depositAmount)+int(fields[rowNum][9]))
     fields[rowNum][9] = goalProgress
+    fields[rowNum][11] = str(datetime.now().day) + "\n"
     print("You deposited ${} into your savings account and only need ${} to reach your money management goal!".format(depositAmount, int(fields[rowNum][2]) - int(fields[rowNum][9])))
     
   accountFile = open('clients.txt','w')
 
-  clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days\n'
+  clientList = 'Username | Password | Goal Balance | Year | Month | Day | Bank Number | Savings Balance | Chequing Balance | Money Towards Goal | Goal Days | Last Plan Deposit Day\n'
   for row in range (len(fields)):
-    for column in range(11):
-      if column != 10:
+    for column in range(12):
+      if column != 11:
         clientList += fields[row][column] + ' '
       else:
         clientList+= fields[row][column]
   accountFile.write(clientList)
   accountFile.close()
+
